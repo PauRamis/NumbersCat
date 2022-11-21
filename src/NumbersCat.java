a4d743b00f664c3fd06a9f049ce4fcbbfffd5ac0
Your Name <you@example.com>
2022-11-21 12:07:27 +0100

    des

--- src/NumbersCat.java
+++ src/NumbersCat.java
@@ -2,14 +2,19 @@
 public class NumbersCat {
 
     public static String say(long n) {
+        if (n == 0)
+            return "Zero";
+        String resultat = "";
         /*Pasam el numero a un array d'strings
         String number = String.valueOf(n);
         String[] numberDigits = number.split("(?<=.)");*/
 
-        
 
-        String resultat = "";
-        if (n < 20)
+        if (Math.abs(n) < 100) {
+            resultat = desenes(n);
+            resultat += nums0_19(n);
+        }
+        if (Math.abs(n) < 20)
             resultat = nums0_19(n);
 
         if (n < 0)
@@ -17,9 +22,18 @@ public class NumbersCat {
         return resultat.substring(0, 1).toUpperCase() + resultat.substring(1);
     }
 
+    private static String desenes(long n) {
+        String[] desenes = {
+                "vint", "trenta", "quaranta", "cinquanta", "seixanta", "setanta", "vuitanta", "noranta"
+        };
+        //Només volem la desena del nombre en positiu. Ex: -33 -> 3
+        n = Math.abs(n / 10) - 2;
+        return desenes[(int) n];
+    }
+
     private static String nums0_19(long n) {
         String[] unicNums = {
-                "zero", "un", "dos", "tres", "quatre",
+                "", "un", "dos", "tres", "quatre",
                 "cinc", "sis", "set", "vuit", "nou", "deu", "onze", "dotze", "tretze", "catorze", "quinze", "setze",
                 "disset", "divuit", "dinou"
         };
--- src/Proves.java
+++ src/Proves.java
@@ -1,10 +1,7 @@
 public class Proves {
     public static void main(String[] args) {
-        int n = 123;
-        String number = String.valueOf(n);
-        String[] nDigits = number.split("(?<=.)");
-        for (int i = 0; i < number.length(); i++) {
-            System.out.println(nDigits[i]);
-        }
+        int n = 129;
+        int N = n /10;
+        System.out.println(N);
     }
 }
